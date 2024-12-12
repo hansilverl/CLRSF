cdc259f3e1e65d9c6c9fa8d8da0ea478e89cbf9f
Lilach169 <lilachnachum4@gmail.com>
2024-12-12 14:57:52 +0200

    sln add

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CLRSHFT_compare.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
