f32f0c7d11731ddc9dbc2d7bc0e0e358848b57ca
Administrator <admin@example.com>
2025-01-14 10:14:47 +0200

    refactor input model and update layout title; add amount input field

--- Models/InputModel.cs
+++ Models/InputModel.cs
@@ -10,13 +10,3 @@ namespace CLSF_Compare.Models
         public decimal BankFees { get; set; }
     }
 }
-    /* TODO:
-    * ask mentor how different fees work, some may very by amount, 
-    * Are we calculating by net or sale gross . 
-    * Clearshift fee is a percentage of the amount
-    * Should we consider transactions under 3600 ILS? - What's the minimum amount for a transaction?
-    * Is the clearshift fee (over 3600 a percentage or a flat fee(180)) 
-    * also, will sometimes have to multiply to exchange rate, sometimes divide
-    * Bank formula (with hidden fees) - need to find out HOW to take fees - there may be a few fees to consider for the bank
-    * Clearshift formula is amount * rate - fees
-    */
