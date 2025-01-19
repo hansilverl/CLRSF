5433edf8171ca4936d0439c2760b24ada2d9d856
Administrator <admin@example.com>
2025-01-19 19:02:11 +0200

    Refactor conversion calculation model and update views to reflect new property names for converted amounts

--- Models/ConversionCalculationModel.cs
+++ Models/ConversionCalculationModel.cs
@@ -2,20 +2,26 @@ namespace CLSF_Compare.Models
 {
     public class ConversionCalculationModel
     {
-        public decimal BankConversionCost { get; set; }
-        public decimal ClearShiftConversionCost { get; set; }
+        public decimal BankConvertedAmount { get; set; }
+        public decimal ClearShiftConvertedAmount { get; set; }
         public decimal Savings { get; set; }
 
         public static ConversionCalculationModel Calculate(decimal amount, decimal bankRate, decimal bankFees, decimal clearShiftRate)
         {
-            var bankCost = amount * bankRate + bankFees;
-            var clearShiftCost = amount * clearShiftRate;
+            bankFees = bankFees == 0 ? 1.25m : bankFees;
+            var b_ConvertedAmount = amount * bankRate;
+            var cs_ConvertedAmount = amount * clearShiftRate;
+
+            /* Because rate exchange will usually be higher than bank rate,
+             So by using ClearShift we "gain"/save cs_ConvertedAmount - b_ConvertedAmount
+             we also need to take into account the fees, so we need to subtract the fees from the "gain"  */
+            var savings = cs_ConvertedAmount - b_ConvertedAmount - bankFees;
 
             return new ConversionCalculationModel
             {
-                BankConversionCost = bankCost,
-                ClearShiftConversionCost = clearShiftCost,
-                Savings = bankCost - clearShiftCost
+                BankConvertedAmount = b_ConvertedAmount,
+                ClearShiftConvertedAmount = cs_ConvertedAmount,
+                Savings = savings
             };
         }
     }
