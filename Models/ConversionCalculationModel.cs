8768425c0270968eeb514e219f4a6b3164e64882
Administrator <admin@example.com>
2025-01-19 21:23:31 +0200

    Update conversion calculation to include default bank fees and clearShift fees for accurate savings computation

--- Models/ConversionCalculationModel.cs
+++ Models/ConversionCalculationModel.cs
@@ -8,14 +8,19 @@ namespace CLSF_Compare.Models
 
         public static ConversionCalculationModel Calculate(decimal amount, decimal bankRate, decimal bankFees, decimal clearShiftRate)
         {
+            // default bank fees to 1.25% if not provided
             bankFees = bankFees == 0 ? 1.25m : bankFees;
             var b_ConvertedAmount = amount * bankRate;
             var cs_ConvertedAmount = amount * clearShiftRate;
 
+            var b_Fees = amount * bankFees;
+            // clearShift has one fee of 0.25%
+            var cs_Fees = amount * (0.25m / 100);
+
             /* Because rate exchange will usually be higher than bank rate,
              So by using ClearShift we "gain"/save cs_ConvertedAmount - b_ConvertedAmount
              we also need to take into account the fees, so we need to subtract the fees from the "gain"  */
-            var savings = cs_ConvertedAmount - b_ConvertedAmount - bankFees;
+            var savings = (cs_ConvertedAmount - b_ConvertedAmount) - (b_Fees - cs_Fees);
 
             return new ConversionCalculationModel
             {
