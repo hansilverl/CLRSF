384fa709d415513e7478e93296fd56c867a0ccc4
Lilach169 <lilachnachum4@gmail.com>
2025-04-28 13:39:01 +0300

    שמירה זמנית

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CLSF_Compare.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadCsv(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("No file selected.");
+            }
+
+            try
+            {
+                List<string[]> csvData = new List<string[]>();
+
+                using (var stream = new StreamReader(file.OpenReadStream()))
+                {
+                    while (!stream.EndOfStream)
+                    {
+                        var line = await stream.ReadLineAsync();
+                        if (!string.IsNullOrWhiteSpace(line))
+                        {
+                            var values = line.Split(',');
+                            csvData.Add(values);
+                        }
+                    }
+                }
+
+                ViewBag.CsvData = csvData;
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                return BadRequest("Error processing file.");
+            }
+        }
+    }
+}
+
+        
+    
+
+
