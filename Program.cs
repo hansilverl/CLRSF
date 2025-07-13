47c3b8cc88ee6b7c9596ca94a76e8bc01789ae6f
Hansilverl <hannahsilverb@gmail.com>
2025-07-13 21:48:09 +0300

    Enhance PDF viewer component with modernized styling and improved layout

--- Program.cs
+++ Program.cs
@@ -24,6 +24,17 @@ app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthorization();
 
+// Configure static file cache busting
+app.UseStaticFiles(new StaticFileOptions
+{
+    OnPrepareResponse = ctx =>
+    {
+        ctx.Context.Response.Headers["Cache-Control"] = "no-cache, no-store, must-revalidate";
+        ctx.Context.Response.Headers["Pragma"] = "no-cache";
+        ctx.Context.Response.Headers["Expires"] = "0";
+    }
+});
+
 app.MapStaticAssets();
 
 app.MapControllerRoute(
